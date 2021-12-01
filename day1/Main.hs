015fb68401c00b84978c4b822d28596d9d547f6c
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-12-01 22:39:25 +0000

    Refactor and comment day1

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,9 +1,21 @@
 main :: IO ()
 main = do
   lines <- map (\a -> read a :: Int) . lines <$> readFile "day1/day1_input"
-  print $ length $ filter (uncurry (<)) $ zip lines (tail lines)
-  let window_values = map (sum . tripleToList) $ zip3 lines (tail lines) (tail $ tail lines)
-  print $ length $ filter (uncurry (<)) $ zip window_values (tail window_values)
+  print $ firstProblem lines
+  print $ secondProblem lines
+
+-- Counts how many increments there are in the list
+firstProblem :: [Int] -> Int
+firstProblem list = length $ filter (uncurry (<)) $ zip list (tail list)
+
+-- Counts how many increments there are, but doesn't directly use the
+-- contents of the list, instead it gets a new list with the summed values
+-- of sucessive windows of size three
+secondProblem :: [Int] -> Int
+secondProblem list =
+  let windows = zip3 list (tail list) (tail $ tail list)
+      window_values = map (sum . tripleToList) windows
+   in length $ filter (uncurry (<)) $ zip window_values (tail window_values)
 
 tripleToList :: (a, a, a) -> [a]
 tripleToList (x, y, z) = [x, y, z]
