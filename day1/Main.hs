8fda04583a01b337a299b2758e18f89d7089c5c2
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-12-01 22:15:54 +0000

    Second part of day1

--- day1/Main.hs
+++ day1/Main.hs
@@ -2,3 +2,8 @@ main :: IO ()
 main = do
   lines <- map (\a -> read a :: Int) . lines <$> readFile "day1/day1_input"
   print $ length $ filter (uncurry (<)) $ zip lines (tail lines)
+  let window_values = map (sum . tripleToList) $ zip3 lines (tail lines) (tail $ tail lines)
+  print $ length $ filter (uncurry (<)) $ zip window_values (tail window_values)
+
+tripleToList :: (a, a, a) -> [a]
+tripleToList (x, y, z) = [x, y, z]
