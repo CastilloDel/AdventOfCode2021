c31cd15f5b582cc09a4b6acc278ca7d416eab4a7
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-12-01 22:15:39 +0000

    First part of day1

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,2 +1,4 @@
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  lines <- map (\a -> read a :: Int) . lines <$> readFile "day1/day1_input"
+  print $ length $ filter (uncurry (<)) $ zip lines (tail lines)
