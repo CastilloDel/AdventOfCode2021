8170e5356249cafdede421ff946b0b6dc7429d9f
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-12-05 19:46:30 +0000

    Add solution to the second part of day3

--- day3/src/Main.hs
+++ day3/src/Main.hs
@@ -5,6 +5,7 @@ main :: IO ()
 main = do
   lines <- lines <$> readFile "day3/input"
   print $ firstProblem lines
+  print $ secondProblem lines
 
 firstProblem :: [String] -> Int
 firstProblem lines = binaryToDecimal gamma * binaryToDecimal epsilon
@@ -13,13 +14,34 @@ firstProblem lines = binaryToDecimal gamma * binaryToDecimal epsilon
     epsilon = getEpsilonFromGamma gamma
 
 getGamma :: [String] -> String
-getGamma = foldr (\a acc -> mostFrequent a : acc) "" . transpose
+getGamma = foldr (\val acc -> mostFrequent val : acc) "" . transpose
 
 mostFrequent :: String -> Char
 mostFrequent = head . maximumBy (\a b -> length a `compare` length b) . group . sort
 
 getEpsilonFromGamma :: String -> String
-getEpsilonFromGamma = map (\a -> if a == '0' then '1' else '0')
+getEpsilonFromGamma = map (\val -> if val == '0' then '1' else '0')
+
+secondProblem :: [String] -> Int
+secondProblem lines = oxygenRating * co2Rating
+  where
+    oxygenRating = binaryToDecimal $ getOxygenRating lines
+    co2Rating = binaryToDecimal $ getCO2Rating lines
+
+getOxygenRating :: [String] -> String
+getOxygenRating = getRating mostFrequent
+
+getCO2Rating :: [String] -> String
+getCO2Rating = getRating leastFrequent
+  where
+    leastFrequent = \val -> if mostFrequent val == '0' then '1' else '0'
+
+getRating :: (String -> Char) -> [String] -> String
+getRating _ [x] = x
+getRating criteria lines = val : rest
+  where
+    val = criteria $ head $ transpose lines
+    rest = getRating criteria $ map tail $ filter (\line -> head line == val) lines
 
 binaryToDecimal :: String -> Int
 binaryToDecimal = foldl (\acc digit -> acc * 2 + digitToInt digit) 0
