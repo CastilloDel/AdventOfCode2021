5040a98c673acc1ea3cd44c67e905c384ae49329
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-12-07 13:23:52 +0000

    Add some comments and a Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Solutions to Advent Of Code 2021
+### Daniel del Castillo de la Rosa
+
+This year I decided to participate in the Advent Of Code while learning Haskell.
+All the solutions can be found in their respective directory and in one file. Solutions assume knowledge about the problem itself, you can check the problems in the [Advent Of Code page](https://adventofcode.com). Remember this repository is for the 2021 edition!
--- day7/src/Main.hs
+++ day7/src/Main.hs
@@ -15,12 +15,17 @@ main = do
 firstProblem :: [Int] -> Int
 firstProblem positions = sum $ map (\a -> abs $ a - center) positions
   where
+    -- The center in the first problem can be seen as the median
     center = sort positions !! (length positions `div` 2)
 
 secondProblem :: [Int] -> Int
 secondProblem positions = minimum (map (`calculateDeviationSum` positions) possibleCenters)
   where
     calculateDeviationSum center = sum . map (sumAdditional . abs . (+ (- center)))
+    -- The center in the second problem can be seen as the mean
+    -- We need to check both values because the actual fractional mean is between them
+    -- Rounding won't work, because we are dealing with Integers and we lose some information,
+    -- so the real mean might not be entirely accurate
     possibleCenters = [mean, mean + 1]
     mean = div (sum positions) (length positions)
     sumAdditional val = sum [1 .. val]
