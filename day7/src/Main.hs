abdd54213b3ae4c926a288ae4c7adc550a938c8d
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-12-07 10:03:31 +0000

    Add solution for second part of day7

--- day7/src/Main.hs
+++ day7/src/Main.hs
@@ -1,11 +1,14 @@
 import Data.List (sort)
+import Debug.Trace (trace)
 
 main :: IO ()
 main = do
   testNumbers <- map read . splitByCommas . head . lines <$> readFile "day7/test_input"
   numbers <- map read . splitByCommas . head . lines <$> readFile "day7/input"
-  print $ firstProblem testNumbers == 37
+  print $ "Test input: " ++ show (firstProblem testNumbers == 37)
   print $ firstProblem numbers
+  print $ "Test input: " ++ show (secondProblem testNumbers == 168)
+  print $ secondProblem numbers
   where
     splitByCommas = words . map (\a -> if a == ',' then ' ' else a)
 
@@ -13,3 +16,11 @@ firstProblem :: [Int] -> Int
 firstProblem positions = sum $ map (\a -> abs $ a - center) positions
   where
     center = sort positions !! (length positions `div` 2)
+
+secondProblem :: [Int] -> Int
+secondProblem positions = minimum (map (`calculateDeviationSum` positions) possibleCenters)
+  where
+    calculateDeviationSum center = sum . map (sumAdditional . abs . (+ (- center)))
+    possibleCenters = [mean, mean + 1]
+    mean = div (sum positions) (length positions)
+    sumAdditional val = sum [1 .. val]
