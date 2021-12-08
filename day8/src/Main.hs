96a15981f9fc1cc609229a53282fd2bc72a3fc85
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-12-08 15:47:05 +0000

    Add solution to second part of day 8

--- day8/src/Main.hs
+++ day8/src/Main.hs
@@ -1,8 +1,6 @@
-import qualified Data.Bifunctor
-
--- usedSegments !! 2 equals the number of segments
--- used by a two in a seven segmen display
-usedSegments = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
+import qualified Data.Bifunctor as Bifunctor
+import Data.List (findIndex, (\\))
+import Debug.Trace (trace)
 
 main :: IO ()
 main = do
@@ -10,17 +8,55 @@ main = do
   input <- readInput "day8/input"
   print $ "Test input: " ++ show (firstProblem testInput) ++ " == 26"
   print $ "Problem input: " ++ show (firstProblem input) ++ " == 321"
+  print $ "Test input: " ++ show (secondProblem testInput) ++ " == 61229"
+  print $ "Problem input: " ++ show (secondProblem input) ++ " == 1028926"
   where
     readInput file = map (separateByBar . words) . lines <$> readFile file
-    separateByBar = Data.Bifunctor.second tail . break (== "|")
+    separateByBar = Bifunctor.second tail . break (== "|")
 
 firstProblem :: [([String], [String])] -> Int
 firstProblem = sum . map (length . filterUniqueSegmentNumber . map length . snd)
   where
     filterUniqueSegmentNumber = filter hasUniqueSegmentNumber
-    hasUniqueSegmentNumber num
-      | num == usedSegments !! 1 = True
-      | num == usedSegments !! 4 = True
-      | num == usedSegments !! 7 = True
-      | num == usedSegments !! 8 = True
-      | otherwise = False
+    -- In some cases the number of segments used is unique and can identify the number
+    hasUniqueSegmentNumber 2 = True -- 2 segments -> 1
+    hasUniqueSegmentNumber 4 = True -- 4 segments -> 4
+    hasUniqueSegmentNumber 3 = True -- 3 segments -> 7
+    hasUniqueSegmentNumber 7 = True -- 7 segments -> 8
+    hasUniqueSegmentNumber _ = False
+
+secondProblem :: [([String], [String])] -> Int
+secondProblem = sum . map decodeSegments
+
+decodeSegments :: ([String], [String]) -> Int
+decodeSegments =
+  toInt . map throwMaybes . decodeOutput . Bifunctor.first identifyNumbers
+  where
+    toInt it = read (foldl1 (++) $ map show it) :: Int
+    decodeOutput (numbers, output) =
+      map (\a -> findIndex (isSetEqual a) numbers) output
+    isSetEqual a b = b \\ a == "" && a \\ b == ""
+    -- We can get rid of the maybes because we know the numbers in the output
+    -- are in the first part. It is part of the problem
+    throwMaybes = \(Just a) -> a
+
+identifyNumbers :: [String] -> [String]
+identifyNumbers numbers = [zero, one, two, three, four, five, six, seven, eight, nine]
+  where
+    one = getElementWithLength 2 numbers
+    four = getElementWithLength 4 numbers
+    seven = getElementWithLength 3 numbers
+    eight = getElementWithLength 7 numbers
+    six = head . filterByDifference 1 one $ filterByLength 6 numbers
+    nine = head . filterByDifference 0 four $ filterByLength 6 numbers
+    zero = head . filter (\n -> (n /= six) && (n /= nine)) $ filterByLength 6 numbers
+    three = head . filterByDifference 0 one $ filterByLength 5 numbers
+    two = head . filterByDifference 2 four $ filterByLength 5 numbers
+    five = head . filterByDifference 1 six $ filterByLength 5 numbers
+    getElementWithLength len = head . filterByLength len
+    filterByLength len = filter ((== len) . length)
+
+-- Works by calculating set difference between the second argument
+-- and each string in the third
+filterByDifference :: Int -> String -> [String] -> [String]
+filterByDifference diff original = filter ((== diff) . length . (original \\))
