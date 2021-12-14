c4fb5830decfdc36ecf44933c129dfd6391f9a6b
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-12-14 14:09:43 +0000

    Add solution to second part of day12

--- day12/src/Main.hs
+++ day12/src/Main.hs
@@ -1,4 +1,5 @@
 import Data.Char (isLower)
+import Data.List (nub)
 import Data.List.Split (splitOn)
 import Data.Map as M (Map, empty, insertWith, (!))
 
@@ -14,6 +15,8 @@ main = do
   input <- readInput "day12/input"
   print $ "Test input: " ++ show (firstProblem testInput) ++ " == 10"
   print $ "Problem input: " ++ show (firstProblem input) ++ " == 3738"
+  print $ "Test input: " ++ show (secondProblem testInput) ++ " == 36"
+  print $ "Problem input: " ++ show (secondProblem input) ++ " == 120506"
   where
     readInput file = foldl foldLine M.empty . lines <$> readFile file
     foldLine acc = insertNew acc . splitOn "-"
@@ -28,16 +31,32 @@ isSmall = isLower . head
 
 type CaveConections = Map Cave [Cave]
 
-firstProblem :: CaveConections -> Int
-firstProblem = length . getPaths
+type Path = [Cave]
 
-getPaths :: CaveConections -> [[String]]
-getPaths connections = getPaths' ["start"]
+firstProblem :: CaveConections -> Int
+firstProblem = problem caveFilter
+  where
+    caveFilter path cave = not $ isSmall cave && elem cave path
+
+secondProblem :: CaveConections -> Int
+secondProblem = problem caveFilter
+  where
+    caveFilter path cave =
+      let visitedSmallTwice = (\a -> length (nub a) /= length a) $ filter isSmall path
+       in if visitedSmallTwice
+            then not $ isSmall cave && elem cave path
+            else cave /= "start"
+
+problem :: (Path -> Cave -> Bool) -> CaveConections -> Int
+problem predicate = length . getPaths predicate
+
+getPaths :: (Path -> Cave -> Bool) -> CaveConections -> [Path]
+getPaths predicate connections = getPaths' ["start"]
   where
     getPaths' path@("end" : _) = [reverse path]
     getPaths' path =
-      let possibleMovements = filterSmall path $ connections ! head path
+      let possibleMovements = filterByPredicate path $ connections ! head path
        in if null possibleMovements
             then []
             else concatMap (getPaths' . (: path)) possibleMovements
-    filterSmall path = filter $ not . (\cave -> isSmall cave && elem cave path)
+    filterByPredicate path = filter $ predicate path
