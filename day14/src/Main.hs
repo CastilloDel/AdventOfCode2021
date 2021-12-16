85ec83def8b5e144421d33c3568ca2445be206d7
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-12-16 10:26:14 +0000

    Add solution to second part of day14

--- day14/src/Main.hs
+++ day14/src/Main.hs
@@ -1,18 +1,35 @@
 import Data.List (group, sort)
 import Data.List.Split (splitOn)
 import Data.Map (Map, (!))
-import qualified Data.Map as Map (empty, insert)
+import qualified Data.Map as Map (empty, filterWithKey, foldlWithKey, insert, insertWith, map, toList)
+import Debug.Trace (trace)
 
 main :: IO ()
 main = do
   (testTemplate, testRules) <- readInput "day14/test_input"
   (template, rules) <- readInput "day14/input"
   print $ "Test input: " ++ show (firstProblem testTemplate testRules) ++ " == 1558"
-  print $ "Problem input: " ++ show (firstProblem template rules) ++ " == *"
+  print $ "Problem input: " ++ show (firstProblem template rules) ++ " == 3048"
+  print $ "Test input: " ++ show (secondProblem testTemplate testRules) ++ " == 2188189693529"
+  print $ "Problem input: " ++ show (secondProblem template rules) ++ " == 3288891573057"
   where
     readInput file = parseInput . splitOn [""] . lines <$> readFile file
-    parseInput (template : rules : _) = (head template, ruleFromStrings rules)
+    parseInput (template : rules : _) =
+      (polymerFromString $ head template, ruleFromStrings rules)
     parseInput _ = error "Invalid input"
+
+type Polymer = Map (Char, Char) Int
+
+-- Each element is paired twice, with the element on its left and its right.
+-- Converting to Polymer discards the order of the elements,
+-- but it is okay, because we don't need them.
+polymerFromString :: String -> Polymer
+polymerFromString s = foldl foldIntoPolymer Map.empty zippedS
+  where
+    zippedS = zip sWithLimits $ tail sWithLimits
+    -- We add spaces to know which letters are in the borders
+    sWithLimits = ' ' : s ++ [' ']
+    foldIntoPolymer map key = Map.insertWith (+) key 1 map
 
 type Rules = Map (Char, Char) Char
 
@@ -27,16 +44,34 @@ ruleKeyValFromStrings s = fromParts $ splitOn " -> " s
     fromParts (pair : insertion : _) = ((head pair, pair !! 1), head insertion)
     fromParts _ = error $ "Invalid input: " ++ s
 
-firstProblem :: String -> Rules -> Int
-firstProblem polymer rules = max - min
+firstProblem :: Polymer -> Rules -> Int
+firstProblem = problem 10
+
+secondProblem :: Polymer -> Rules -> Int
+secondProblem = problem 40
+
+problem :: Int -> Polymer -> Rules -> Int
+problem steps polymer rules = max - min
   where
     max = maximum counts
     min = minimum counts
-    counts = map length $ group $ sort after5Steps
-    after5Steps = iterate (`applyRules` rules) polymer !! 10
+    -- We need to divide by two because each value is counted twice, left and right
+    -- and we also need to remove the spaces
+    counts = Map.map (`div` 2) $ Map.filterWithKey (\k _ -> k /= ' ') countsMap
+    countsMap = Map.foldlWithKey foldIntoCount Map.empty afterNSteps
+    foldIntoCount counts key val =
+      Map.insertWith (+) (snd key) val $
+        Map.insertWith (+) (fst key) val counts
+    afterNSteps = iterate (`applyRules` rules) polymer !! steps
 
-applyRules :: String -> Rules -> String
+applyRules :: Polymer -> Rules -> Polymer
 applyRules polymer rules =
-  (head polymer :) $ foldr foldIntoNewPolymer "" (zip polymer $ tail polymer)
+  Map.foldlWithKey foldIntoNewPolymer Map.empty polymer
   where
-    foldIntoNewPolymer key new = (rules ! key) : snd key : new
+    foldIntoNewPolymer new key val
+      -- Skip the borders
+      | fst key == ' ' || snd key == ' ' = Map.insert key val new
+      | otherwise =
+        let insertion = rules ! key
+         in Map.insertWith (+) (insertion, snd key) val $
+              Map.insertWith (+) (fst key, insertion) val new
