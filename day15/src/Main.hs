2ee118e4f54993d19adcf2007d3e672bf0afcf46
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-12-16 19:12:15 +0000

    Add solution to second part of day15

--- day15/src/Main.hs
+++ day15/src/Main.hs
@@ -1,8 +1,18 @@
 import Data.Char (digitToInt)
-import Data.Matrix (Matrix, ncols, nrows, (!))
+import Data.Map (Map)
+import qualified Data.Map as Map (empty, findWithDefault, insert)
+import Data.Matrix (Matrix, ncols, nrows, (!), (<->), (<|>))
 import qualified Data.Matrix as Matrix (fromLists)
 import Data.Set (Set)
-import qualified Data.Set as Set (delete, empty, fromList, insert, member, toAscList)
+import qualified Data.Set as Set
+  ( delete,
+    empty,
+    fromList,
+    insert,
+    member,
+    notMember,
+    toAscList,
+  )
 
 main :: IO ()
 main = do
@@ -10,6 +20,8 @@ main = do
   cavern <- readInput "day15/input"
   print $ "Test input: " ++ show (firstProblem testCavern) ++ " == 40"
   print $ "Problem input: " ++ show (firstProblem cavern) ++ " == 429"
+  print $ "Test input: " ++ show (secondProblem testCavern) ++ " == 315"
+  print $ "Problem input: " ++ show (secondProblem cavern) ++ " == *"
   where
     readInput file = fmap digitToInt . Matrix.fromLists . lines <$> readFile file
 
@@ -56,24 +68,39 @@ getTotalCost Path {cost = c, predictedCost = pc} = c + pc
 firstProblem :: CavernMap -> Int
 firstProblem map = getPathCost (1, 1) (nrows map, ncols map) map
 
+secondProblem :: CavernMap -> Int
+secondProblem cavern =
+  getPathCost (1, 1) (nrows wholeCavern, ncols wholeCavern) wholeCavern
+  where
+    wholeCavern = foldl1 (<->) $ map getRow [0 .. 4]
+    getRow n = foldl1 (<|>) $ map (caverns !!) [n .. n + 4]
+    caverns = iterate (fmap addWithOverflow9) cavern
+    addWithOverflow9 9 = 1
+    addWithOverflow9 val = val + 1
+
 getPathCost :: Point -> Point -> CavernMap -> Int
-getPathCost start end cavern = getPathCost' initialPaths
+getPathCost start end cavern = getPathCost' initialPaths Map.empty
   where
     initialPaths = Set.insert (newPath start 0 end) Set.empty
-    getPathCost' paths =
+    getPathCost' paths visited =
       let cheapestPath = head $ Set.toAscList paths
           lastPoint = position cheapestPath
           paths' = Set.delete cheapestPath paths
-          newPaths = getNewPaths cheapestPath end cavern
+          newPaths = getNewPaths cheapestPath end visited cavern
           nextPaths = foldr Set.insert paths' newPaths
+          nextVisited = foldl foldPointsWithCost visited newPaths
+          foldPointsWithCost m path = Map.insert (position path) (cost path) m
        in if lastPoint == end
             then cost cheapestPath
-            else getPathCost' nextPaths
+            else getPathCost' nextPaths nextVisited
 
-getNewPaths :: Path -> Point -> CavernMap -> [Path]
-getNewPaths actual end cavern =
-  map getNewPath $ filter (isValid cavern) $ getNeighbours $ position actual
+getNewPaths :: Path -> Point -> Map Point Int -> CavernMap -> [Path]
+getNewPaths actual end visited cavern =
+  filter (\path -> getPathOldCost path > cost path) possibleNewPaths
   where
+    getPathOldCost path = Map.findWithDefault maxBound (position path) visited
+    possibleNewPaths = map getNewPath $ filter (isValid cavern) neighbours
+    neighbours = getNeighbours $ position actual
     getNewPath next = newPath next (cavern ! next + cost actual) end
 
 getNeighbours :: Point -> [Point]
